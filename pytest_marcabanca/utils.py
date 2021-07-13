948648f043e1f742f1c7925ee0f87a6549eae4e5
Joaquin Zepeda <jazs.pro@gmail.com>
2021-07-12 23:01:48 -0700

    Utils with Manager class in progress.

--- pytest_marcabanca/utils.py
+++ pytest_marcabanca/utils.py
@@ -1,7 +1,14 @@
 import uuid
+from contextlib import ExitStack
+from pglib.validation import checked_get_single
+import numpy as np
+import scipy.stats as scipy_stats
+import warnings
+from pglib.serializer.abstract_type_serializer import (
+    AbstractTypeSerializer as _AbstractTypeSerializer)
+from pglib.serializer import Serializer as _Serializer
 from pglib.filelock import FileLock
-from typing import Union, Dict
-import json
+from typing import Iterable, List, Union
 import sys
 import subprocess as subp
 from cpuinfo import get_cpu_info
@@ -12,94 +19,259 @@ import platform
 from secrets import token_hex
 
 
-class ConfigurationManager:
+def find(obj_id, obj_list: List, id_attr_name):
+    """
+    Finds the object with the specified id in a list of objects.
+    :return: Returns a list of 2-tuples containing the position in the list and object for all matching objects.
+    """
+    return [(_k, _obj) for _k, _obj in enumerate(obj_list)
+            if getattr(_obj, id_attr_name) == obj_id]
 
-    def __init__(self, machine_config_path, python_config_path):
-        self.machine_config_path = machine_config_path
-        self.python_config_path = python_config_path
 
-    def get_conf_id(self):
+class Manager:
+    """
+    Loads all data upon initialization and re-writes it with any updates upon calling :meth:`write`.
+    """
+
+    def __init__(self, machine_configs_path, python_configs_path, references_path):
+
+        serializer = _Serializer()
+        self.paths = {
+            'machine_configs': machine_configs_path,
+            'python_configs': python_configs_path,
+            'references': references_path
+        }
+        self.data = {
+            'machine_configs': serializer.load_safe(machine_configs_path)[0] or [],
+            'python_configs': serializer.load_safe(python_configs_path)[0] or [],
+            'references': serializer.load_safe(references_path)[0] or []}
+
+        # Get this machine's configurations and ensure they exist in data
+        this_machine_config = MachineConfiguration()
+        this_python_config = PythonConfiguration()
+
+        if this_machine_config not in self.data['machine_configs']:
+            self.data['machine_configs'].append(this_machine_config)
+        self.this_machine_config = checked_get_single(
+            [_x for _x in self.data['machine_configs'] if _x == this_machine_config])
+
+        if this_python_config not in self.data['python_configs']:
+            self.data['python_configs'].append(this_python_config)
+        self.this_python_config = checked_get_single(
+            [_x for _x in self.data['python_configs'] if _x == this_python_config])
+
+    def build_reference_id(self, test_node_id):
+        return {'machine_config_id': self.this_machine_config.config_id,
+                'python_config_id': self.python_machine_config.config_id,
+                'test_node_id': test_node_id}
+
+    def compare_runtime(self, test_node_id, runtime):
+        reference_id = self.build_reference_id(test_node_id)
+
+        if (reference := Reference.find(reference_id, self.data['references'])):
+            exact = True
+        elif (references := [_x for _x in self.data['references'] if _x.reference_id['test_node_id'] == test_node_id]):
+            exact = False
+            reference = references[0]
+        else:
+            return None
+
+        return exact, reference.compare(runtime)
+
+    def create_reference(self, test_node_id, runtimes):
+        reference_id = self.build_reference_id(test_node_id)
+        posn, reference = Reference.find(reference_id, self.data['references'])
+        if reference:
+            reference.runtimes = runtimes
+            reference.fit()
+        else:
+            reference = Reference(reference_id, runtimes)
+            self.data['references'].append(reference)
+
+    def write(self):
+        with ExitStack() as stack:
+            [stack.enter_context(FileLock(_x)) for _x in self.paths.values()]
+            [self.serializer.dump(self.data[key], self.path[key]) for key in self.data]
+
+
+class Reference:
+    def __init__(self, reference_id, runtimes, model=None):
+        self.reference_id = reference_id
+        self.runtimes = np.require(runtimes, dtype='f').tolist()
+        self.model = model
+
+    def fit(self):
+        self.model = _ProbModel('gamma')
+        self.model.fit(self.runtimes)
+
+    @classmethod
+    def find(self, reference_id):
         """
-        Returns a JSON-serializable machine and python configuration identifier.
+        Same as :func:`find`, but ensures at most one reference exists. Returns a :class:`Reference` object, and not a list of objects.
         """
-        mach_conf_id = self._get_conf_id_helper('machine',
-                                                MachineConfiguration(),
-                                                self.machine_config_path)
-        py_conf_id = self._get_conf_id_helper('python',
-                                              PythonConfiguration(),
-                                              self.python_config_path)
-        return {'machine_conf_id': mach_conf_id,
-                'python_conf_id': py_conf_id}
+        if (reference := find(reference_id, self.data['references'], 'reference_id')[1]):
+            return checked_get_single(
+                reference,
+                msg=f'Expected 1 but found {{count}} references matching id {reference_id}.')
+        else:
+            return None, None
 
-    def _get_conf_id_helper(self, type_name, this_conf, json_path):
+    def compare(self, x):
+        """
+        Returns the rank of x in the cdf (i.e., percentage of samples lower than x as per the model).
+        """
+        return self.model.cdf(x)
 
-        # Get the configuration identifier.
-        file_lock = FileLock(json_path)
-        with file_lock.with_acquire(create=True):
-            with open(json_path, 'r') as fo:
-                id_conf_pairs = [json.loads(ln) for ln in fo.readline()]
+    @classmethod
+    def _as_serializable(self, obj):
+        if self.model is None:
+            raise Exception('Request to save a model that has not been fit.')
+        return {
+            'reference_id': self.reference_id,
+            'runtimes': self.runtimes,
+            'model': self.model}
 
-            match = [(_id, _conf) for _id, _conf in id_conf_pairs
-                     if this_conf == _conf]
-            if match:
-                # This configuration was already in the file.
-                conf_id = match[0][0]
-            else:
-                # This configuration was not in the file, add it.
-                conf_id = token_hex(16)
-                id_conf_pairs.append(
-                    {f'{type_name}_conf_id': conf_id, f'{type_name}_conf': this_conf.specs})
-                with open(json_path, 'w') as fo:
-                    json.dump(id_conf_pairs, fo, indent=4)
+    @classmethod
+    def _from_serializable(cls, data):
+        return cls(data['reference_id'], data['runtimes'], data['model'])
 
-        return conf_id
 
-        # Get the py env identifier.
+class _ProbModel(_AbstractTypeSerializer):
+    _model_types = {'gamma': scipy_stats.gamma}
+
+    def __init__(self, model_name):
+        self.model_type = self._model_types[model_name]
+        self.model_name = model_name
+        self.model = None
+        self.args = None
+        self.kwargs = None
+
+    def load(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        self.model = self.model_type(*args, **kwargs)
+
+    def fit(self, runtimes):
+        self.args = self.model_type.fit(runtimes)
+        self.kwargs = {}
+        self.model = self.model_type(*self.args)
+
+    def cdf(self, x):
+        if self.model is None:
+            raise Exception('Cannot bcompute a cdf because a model has not been fitted.')
+        return self.model.cdf(x)
+
+    def __eq__(self, obj):
+        if self.model is None or obj.model is None:
+            raise Exception('Cannot compare models because one of the models has not been fitted.')
+        return (
+            self.model_name == obj.model_name and
+            self.args == obj.args and
+            self.kwargs == obj.kwargs)
+
+    @classmethod
+    def _as_serializable(cls, obj):
+        if obj.args is None or obj.kwargs is None:
+            raise Exception('The model cannot be serialized because it has not been fitted.')
+        return {'model_name': obj.model_name,
+                'args': obj.args,
+                'kwargs': obj.kwargs}
+
+    @classmethod
+    def _from_serializable(cls, data):
+        model = cls(data['model_name'])
+        model.load(*data['args'], **data['kwargs'])
+        return model
 
 
 @dataclass
-class _PythonModule:
+class _PythonModule(_AbstractTypeSerializer):
+    _keys = ['package', 'version', 'location']
     package: str
     version: str
     location: str = None
 
+    def __eq__(self, pm):
+        return (
+            self.package == pm.package and
+            self.version == pm.version)
 
-class PythonConfiguration:
-    def __init__(self, specs):
+    def __hash__(self):
+        return hash((self.package, self.version))
+
+    @classmethod
+    def _as_serializable(cls, obj):
+        return {key: getattr(obj, key) for key in cls._keys}
+
+    @classmethod
+    def _from_serializable(cls, data):
+        return cls(**data)
+
+
+class _AbstractConfiguration(_AbstractTypeSerializer):
+    @classmethod
+    def _as_serializable(cls, obj):
+        return {'config_id': obj.config_id, 'specs': obj.specs}
+
+    @classmethod
+    def _from_serializable(cls, data):
+        return cls(config_id=data['config_id'], specs=data['specs'])
+
+    @classmethod
+    def _get_new_id(cls):
+        return token_hex(16)
+
+
+class PythonConfiguration(_AbstractConfiguration):
+    def __init__(self, config_id=None, specs=None):
+        self.config_id = config_id or self._get_new_id()
         self.specs = specs or self._get_specs()
 
-    @ classmethod
+    @classmethod
     def _get_specs(cls):
         return {'python': sys.version,
-                'modules': [_PythonModule(*re.split(r'\s+', _x))
-                            for _x in subp.check_output(['pip', 'list']).split('\n')[2:]]}
+                'modules': [
+                    _PythonModule(*re.split(r'\s+', _x))
+                    for _x in subp.check_output(
+                        ['pip', 'list'], text=True).strip().split('\n')[2:]]}
 
-    def __eq__(self, other):
-        return set(self.specs) == set(other.specs)
+    def __eq__(self, other: 'PythonConfiguration'):
+        return (
+            self.specs['python'] == other.specs['python'] and
+            set(self.specs['modules']) == set(other.specs['modules']))
 
 
-class MachineConfiguration:
+class MachineConfiguration(_AbstractConfiguration):
     """
     Represents the machine's hardware configuration.
     """
 
-    def __init__(self, specs=None, sudo=False):
+    def __init__(self, config_id=None, specs=None):
 
-        specs = specs or self._get_specs(sudo)
+        self.config_id = config_id or self._get_new_id()
+        specs = specs or self._get_specs()
         self.specs = specs
 
-    @ classmethod
+    @classmethod
     def _get_specs(cls):
         return {
             'host': platform.node(),
             'mac_address': cls.get_mac_address(),
             'cpuinfo': get_cpu_info(),
-            'memory': psutil.virtual_memory()['total']
+            'memory': psutil.virtual_memory().total
         }
 
-    @ staticmethod
-    def get_mac_address(sudo=True):
-        return uuid.UUID(int=uuid.getnode())
+    @staticmethod
+    def get_mac_address():
+        return str(uuid.UUID(int=uuid.getnode()))
 
-    def __eq__(self, other: Union['MachineConfiguration', Dict]):
-        return self.specs == getattr(other, 'specs', other)
+    def __eq__(self, other: 'MachineConfiguration'):
+        """
+        Ignores the config_id value.
+        """
+        return self.specs == other.specs
+
+
+# Register type serializers
+_Serializer.default_extension_types.extend([
+    PythonConfiguration, MachineConfiguration, _PythonModule, _ProbModel, Reference])
--- tests/pytest_marcabanca/utils.py
+++ tests/pytest_marcabanca/utils.py
@@ -1,1 +1,101 @@
 import pytest_marcabanca.utils as mdl
+import numpy.testing as npt
+import numpy as np
+import os.path as osp
+from unittest import TestCase
+from pglib.serializer import Serializer
+from tempfile import TemporaryDirectory
+from contextlib import contextmanager
+
+
+class TestMachineConfiguration(TestCase):
+
+    serializer = Serializer()
+
+    def test_serialize(self):
+
+        mc = mdl.MachineConfiguration()
+        self.assertEqual(
+            self.serializer.deserialize(self.serializer.serialize(mc)),
+            mc)
+
+    def test_eq(self):
+        mc1 = mdl.MachineConfiguration()
+        mc2 = mdl.MachineConfiguration()
+        self.assertEqual(mc1, mc2)
+        mc1.specs['cpuinfo']['arch'] = mc1.specs['cpuinfo']['arch'] + '_different'
+        self.assertNotEqual(mc1, mc2)
+
+
+class TestPythonConfiguration(TestCase):
+
+    serializer = Serializer()
+
+    def test_serialize(self):
+
+        mc = mdl.PythonConfiguration()
+        self.assertEqual(
+            self.serializer.deserialize(self.serializer.serialize(mc)),
+            mc)
+
+    def test_eq(self):
+        pc1 = mdl.PythonConfiguration()
+        pc2 = mdl.PythonConfiguration()
+        self.assertEqual(pc1, pc2)
+        pc1.specs['modules'][0].version = pc1.specs['modules'][0].version+'_different'
+        self.assertNotEqual(pc1, pc2)
+
+
+@contextmanager
+def get_references_manager():
+    with TemporaryDirectory() as temp_dir:
+        rm = mdl.ReferencesManager(osp.join(temp_dir, 'machines.json'),
+                                   osp.join(temp_dir, 'python.json'),
+                                   osp.join(temp_dir, 'references.json'))
+        yield rm
+
+
+class TestReferencesManager(TestCase):
+    def test_retrieve_or_store_this_env_config_id(self):
+        with get_references_manager() as rm:
+            env_id = rm.retrieve_or_store_this_env_config_id()
+            [self.assertTrue(
+                isinstance(env_id[_conf_type], str) and len(env_id[_conf_type]) == 32)
+             for _conf_type in ['machine_config_id', 'python_config_id']]
+
+            self.assertEqual(
+                mdl.MachineConfiguration(),
+                rm._retrieve_config_from_id('machine', env_id['machine_config_id']))
+
+            self.assertEqual(
+                mdl.PythonConfiguration(),
+                rm._retrieve_config_from_id('python', env_id['python_config_id']))
+
+    def test_create_write_reference(self):
+        with get_references_manager() as rm:
+            self.assertEqual(rm.get_retrieved_references(), [])
+            rm.create_reference('my.module::MyClass::my_meth', np.linspace(0, 1.0, 10))
+            self.assertNotEqual(rm.get_retrieved_references(), [])
+            rm.write_references()
+
+            rm2 = mdl.ReferencesManager(
+                machine_configs_path=rm.config_paths['machine'],
+                python_configs_path=rm.config_paths['python'],
+                references_path=rm.config_paths['references'])
+
+            self.assertEqual(
+                rm.get_retrieved_references(),
+                rm2.get_retrieved_references())
+
+
+class Test_ProbModel(TestCase):
+    def test_serialize(self):
+        runtimes = np.linspace(0, 1.0, 10)
+        pm = mdl._ProbModel('gamma')
+        pm.fit(runtimes)
+        cdf = pm.cdf(runtimes)
+
+        serializer = Serializer()
+        npt.assert_array_equal(
+            serializer.deserialize(serializer.serialize(pm)).cdf(runtimes),
+            cdf)
